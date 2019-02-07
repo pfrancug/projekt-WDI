1891631b2efda63d645a5fc5d427abd82c8ab964
Piotr Francug <piotr.francug@gmail.com>
2019-02-07 16:51:06 +0100

    comments

--- functions.h
+++ functions.h
@@ -1,10 +1,10 @@
 #ifndef PROJEKT_WDI_FUNCTIONS_H
 #define PROJEKT_WDI_FUNCTIONS_H
 
-// Liczba elementów w ciągu
+/// Liczba elementów w ciągu
 #define LEN 10
 
-// Wyświetlenie menu wyboru
+/// Wyświetlenie menu wyboru
 void menu() {
     printf("\n\tMenu główne\n\n");
     printf("1. Wprowadzenie ciągu 10 liczb całkowitych\n");
@@ -14,20 +14,13 @@ void menu() {
     printf("5. Wyjście z programu\n");
 }
 
-// Czyszczenie bufora
+/// Czyszczenie bufora
 void clearBuffer() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF) {}
 }
 
-// Czekaj na Enter
-void waitEnter() {
-    printf("\nNaciśnij enter, aby powrócić do menu głównego...");
-    clearBuffer();
-    getchar();
-}
-
-// Wyświetlenie ciągu liczb
+/// Wyświetlenie ciągu liczb
 void printArr(const int *numbersArr, char *text) {
     printf("%s ciąg:", text);
     for (int i = 0; i < LEN; i++) {
@@ -36,7 +29,7 @@ void printArr(const int *numbersArr, char *text) {
     printf("\n");
 }
 
-// Wprowadzenie ciągu znaków
+/// Wprowadzenie ciągu znaków
 void updateArr(const int *numbersArr, int start) {
     for (int i = start; i < LEN; i++) {
         printf("Wprowadź %i. liczbę:\n", i + 1);
@@ -53,7 +46,7 @@ void updateArr(const int *numbersArr, int start) {
     getchar();
 }
 
-// Sprawdzenie czy uzupełniono ciąg liczbowy
+/// Sprawdzenie czy uzupełniono ciąg liczbowy
 void checkArr(const int *numbersArr) {
     if (strlen(numbersArr) == 0) {
         system("clear");
@@ -63,7 +56,7 @@ void checkArr(const int *numbersArr) {
     system("clear");
 }
 
-// Sortowanie liczb ciągu w porządku rosnącym
+/// Sortowanie liczb ciągu w porządku rosnącym
 void sortArr(const int *numbersArr) {
     int sortedArr[LEN];
     for (int i = 0; i < LEN; i++) {
